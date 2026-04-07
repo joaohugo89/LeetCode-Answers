6166aa631d229d0f3a4743ff153a595bc0ed262b
joaohugo89 <69850620+joaohugo89@users.noreply.github.com>
2026-04-07 16:08:07 -0300

    Time: 24 ms (5.94%) | Memory: 42.6 MB (50.00%) - LeetSync

--- LinkedList/Easy/69-sqrtx/sqrtx.java
+++ LinkedList/Easy/69-sqrtx/sqrtx.java
@@ -1,5 +1,7 @@
 class Solution {
     public int mySqrt(int x) {
+        if (x == 0) return 0;
+        if (x < 2) return 1;
         int i = 0;
         while ((long) i * i <= x) {
             i++;
