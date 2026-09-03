cafdaeeca6605a112c82124b132b2e22b4db4eb3
joaohugo89 <69850620+joaohugo89@users.noreply.github.com>
2026-09-02 23:52:41 -0300

    Time: 20 ms (75.39%) | Memory: 57.2 MB (99.70%) - LeetSync

---
+++ LinkedList/Medium/146-lru-cache/lru-cache.java
@@ -0,0 +1,54 @@
+class LRUCache {
+    class Node {
+        int key, value;
+        Node prev, next;
+        Node(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    private Map<Integer, Node> map = new HashMap<>();
+    private Node head = new Node(0, 0);
+    private Node tail = new Node(0, 0);
+    private int capacity;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    public int get(int key) {
+        if (!map.containsKey(key)) return -1;
+        Node node = map.get(key);
+        remove(node);
+        insert(node);
+        return node.value;
+    }
+
+    public void put(int key, int value) {
+        if (map.containsKey(key)) {
+            remove(map.get(key));
+        }
+        if (map.size() == capacity) {
+            remove(tail.prev);
+        }
+        insert(new Node(key, value));
+    }
+
+    private void remove(Node node) {
+        map.remove(node.key);
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+
+    private void insert(Node node) {
+        map.put(node.key, node);
+        Node headNext = head.next;
+        head.next = node;
+        node.prev = head;
+        node.next = headNext;
+        headNext.prev = node;
+    }
+}
